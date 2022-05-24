1329d2ea2fda6b3dd366dc98ae05a3e43b7a8946
Viktory23 <106154723+Viktory23@users.noreply.github.com>
2022-05-24 18:23:37 +0300

    Add files via upload

---
+++ pythonProject9/.idea/main.py
@@ -0,0 +1,11 @@
+a = input("write the number")
+b = input("write the number")
+c = input("write the number")
+if a > b:
+    print ("So sad.")
+if b > a and b > c:
+    print("Very very HAPPY!")
+else:
+    print("Not so good.")
+if a == b:
+    print("Oh, the same.")
